18e17ae35eea6a4cfd622c3953f94699e1587c60
martensonbj <martensonbj@gmail.com>
2016-10-24 18:18:15 -0600

    Router wired up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
+
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import { BrowserRouter, Match, Miss } from 'react-router'
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={App} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root />, document.querySelector("#root"))
